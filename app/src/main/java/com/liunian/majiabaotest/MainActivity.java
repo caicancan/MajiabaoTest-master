5cb37d283bb3ea4543ba2b46bafd5731ce56c0f1
caicancan <18062411472@163.com>
2018-05-24 17:33:20 +0800

    Initial commit

---
+++ app/src/main/java/com/liunian/majiabaotest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.liunian.majiabaotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.tv);
+        textView.setText(getPackageName());
+    }
+}
